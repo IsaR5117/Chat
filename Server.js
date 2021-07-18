83073eaec9f9773f4bbfa4e910dff74e9222d720
Isabela Ramirez <ir2416724@gmail.com>
2021-07-17 19:39:55 -0500

    Creacion de socket tcp para recepcion de datos

---
+++ Server.js
@@ -0,0 +1,20 @@
+const net = require('net');
+const server = net.createServer();
+server.on('connection', (socket)=>{
+    socket.on('data', (data)=>{
+        console.log('Mensaje recibido desde el cliente: '+ data);
+    })
+
+    socket.on('close', ()=>{
+        console.log('Comunicacion finalizada')
+    })
+
+    socket.on('error', (err)=>{
+        console.log(err.message)
+    }) 
+    
+})
+
+server.listen(3006, ()=>{
+    console.log('El servidor esta escuchando por el puerto ',server.address().port);
+})
