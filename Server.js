71e2e4e7e3c9e707f016e80e825d8c9fac35e032
AdrianSanchez <electrocamo@gmail.com>
2021-07-27 13:42:47 -0500

    Cambio en el servidor para que responda los mensajes que recibe

--- Server.js
+++ Server.js
@@ -1,8 +1,13 @@
 const net = require('net');
+
 const server = net.createServer();
+
+
+
 server.on('connection', (socket)=>{
     socket.on('data', (data)=>{
-        console.log('Mensaje recibido desde el cliente: '+ data);
+        console.log('Mensaje recibido desde el cliente: '+ socket.remoteAddress + ' Por el puerto: ' + socket.remotePort + ' Info: ' + data);
+        socket.write('Respondo');
     })
 
     socket.on('close', ()=>{
@@ -12,12 +17,14 @@ server.on('connection', (socket)=>{
     socket.on('error', (err)=>{
         console.log(err.message)
     }) 
-    
-})
-function isabelaramirez () {
-    console.log ("Isabela y DiegoD_dllo 'chat'")
-}
 
-server.listen(3006, ()=>{
-    console.log('El servidor esta escuchando por el puerto ',server.address().port);
+   /*  setInterval(()=>{
+        socket.write('Hola, como estas');
+    },6000); */
+
+})
+
+
+server.listen(4004, ()=>{
+console.log('El servidor esta escuchando por el puerto ',server.address().port);
 })
